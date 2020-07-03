4d59072213eed5df51d4f909b3137bc729bea3c4
Mrhu <>
2020-07-03 18:07:21 +0800

    a

--- Desktop/CopyToMyApp/CopyToMyApp/ViewController.m
+++ Desktop/CopyToMyApp/CopyToMyApp/ViewController.m
@@ -8,16 +8,25 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIDocumentInteractionControllerDelegate>
+
+@property (nonatomic,strong)UIDocumentInteractionController * document;
 
 @end
 
 @implementation ViewController
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fileNotification:) name:@"FileNotification" object:nil];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fileNotification:) name:@"FileNotification" object:nil];
+    // Do any additional setup after loading the view.  
 }
 
 - (void)fileNotification:(NSNotification *)notifcation {
@@ -28,16 +37,77 @@
     // /private/var/mobile/Containers/Data/Application/83643509-E90E-40A6-92EA-47A44B40CBBF/Documents/Inbox/jfkdfj123a.pdf
     NSString *filePath = [info objectForKey:@"filePath"];
 
-    NSLog(@"fileName=%@---filePath=%@", fileName, filePath);
+    //NSLog(@"fileName=%@---filePath=%@", fileName, filePath);
     
     
-    UIAlertController * alert = [UIAlertController alertControllerWithTitle:fileName message:filePath preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction * action = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-        
+    UIAlertController * alert = [UIAlertController alertControllerWithTitle:fileName message:filePath preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction * action1 = [UIAlertAction actionWithTitle:@"预览" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self finallFilePathWithFilePath:filePath index:0];
     }];
-    [alert addAction:action]; 
+    UIAlertAction * action2 = [UIAlertAction actionWithTitle:@"other open" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        [self finallFilePathWithFilePath:filePath index:1];
+    }];
+    [alert addAction:action1];
+    [alert addAction:action2];
     [self presentViewController:alert animated:YES completion:nil];
-    
+}
+
+- (void) finallFilePathWithFilePath:(NSString * )fileUrl index:(NSInteger) index{
+    NSArray *paths1 = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *cachesDir = [[paths1 objectAtIndex:0] stringByAppendingPathComponent:fileUrl.lastPathComponent];
+    NSError*error =nil;
+    BOOL isCopySuccess = [[NSFileManager defaultManager] copyItemAtPath:fileUrl toPath:cachesDir error:&error];
+    if (isCopySuccess) {
+        self.document = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:cachesDir]];
+        self.document.delegate = self;
+        //self.document.UTI = @"com.microsoft.word.doc";
+        if (index == 0) {
+            // 用户预览文件，如图1所示
+            bool canPreView = [self.document presentPreviewAnimated:YES];
+            if(!canPreView) {NSLog(@"无法预览");}
+        }else{
+            // 用户不预览文件直接分享，如图2所示
+            BOOL canOpen = [self.document presentOpenInMenuFromRect:self.view.bounds inView:self.view animated:YES];
+            if(!canOpen) {NSLog(@"沒有程序可以打开选中的文件");}
+        }
+    }
+}
+
+-(UIViewController*)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController*)controller{
+    return
+    self;
+}
+
+-(UIView*)documentInteractionControllerViewForPreview:(UIDocumentInteractionController*)controller {
+    return
+    self.view;
+}
+
+- (CGRect)documentInteractionControllerRectForPreview:(UIDocumentInteractionController*)controller {
+    return
+    self.view.frame;
+}
+
+//点击预览窗口的“Done”(完成)按钮时调用
+
+- (void)documentInteractionControllerDidEndPreview:(UIDocumentInteractionController*)controller {
+
+}
+
+// 文件分享面板弹出的时候调用
+
+- (void)documentInteractionControllerWillPresentOpenInMenu:(UIDocumentInteractionController*)controller{
+    NSLog(@"WillPresentOpenInMenu");
+}
+
+// 当选择一个文件分享App的时候调用
+- (void)documentInteractionController:(UIDocumentInteractionController*)controller willBeginSendingToApplication:(nullable NSString*)application{
+    NSLog(@"begin send : %@", application);
+}
+
+// 弹框消失的时候走的方法
+- (void)documentInteractionControllerDidDismissOpenInMenu:(UIDocumentInteractionController*)controller{
+    NSLog(@"dissMiss");
 }
 
 - (void)dealloc{
